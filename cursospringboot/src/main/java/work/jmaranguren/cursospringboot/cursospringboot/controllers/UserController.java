fa42f10f72c7e2ea93ff8de15928e792b8b5337b
Jose Maria Aranguren <64466787+josema294@users.noreply.github.com>
2024-06-18 22:43:09 +0200

    [clase 23]

--- cursospringboot/src/main/java/work/jmaranguren/cursospringboot/cursospringboot/controllers/UserController.java
+++ cursospringboot/src/main/java/work/jmaranguren/cursospringboot/cursospringboot/controllers/UserController.java
@@ -1,11 +1,15 @@
 package work.jmaranguren.cursospringboot.cursospringboot.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 import work.jmaranguren.cursospringboot.cursospringboot.models.User;
+
 
 @Controller
 public class UserController {
@@ -28,6 +32,17 @@ public class UserController {
        
         return "details";
     }
+
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+
+        List <User> users = new ArrayList<>();
+        
+        model.addAttribute("title","Lista");
+        model.addAttribute("users", users);
+        return "list";
+    }
+    
 
     
     
