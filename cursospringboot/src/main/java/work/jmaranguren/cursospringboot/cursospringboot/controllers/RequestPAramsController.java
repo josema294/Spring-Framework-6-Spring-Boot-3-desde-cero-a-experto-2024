8f43076ca8d0c5a9fdbd00bfae1d1e1f55e3a6d9
Jose Maria Aranguren <64466787+josema294@users.noreply.github.com>
2024-07-16 21:15:08 +0200

    [clase 26]

---
+++ cursospringboot/src/main/java/work/jmaranguren/cursospringboot/cursospringboot/controllers/RequestPAramsController.java
@@ -0,0 +1,28 @@
+package work.jmaranguren.cursospringboot.cursospringboot.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import work.jmaranguren.cursospringboot.cursospringboot.models.dto.ParamDTO;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api/params")
+public class RequestPAramsController {
+
+    @GetMapping("/foo")
+    public ParamDTO foo(@RequestParam(required = false, defaultValue = "hola que tal") String message) {
+        
+        var param = new ParamDTO();
+        //Con el defaultvalue ya no hace falta comprobar si es nulo o no
+        //param.setMessage(message==null?"Hola no hay parametro que pasar":message);
+        param.setMessage(message==null?"Hola no hay parametro que pasar":message);
+
+        return param;
+    }
+    
+    
+}
---
+++ cursospringboot/src/main/java/work/jmaranguren/cursospringboot/cursospringboot/models/dto/ParamDTO.java
@@ -0,0 +1,18 @@
+package work.jmaranguren.cursospringboot.cursospringboot.models.dto;
+
+public class ParamDTO {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    
+}
