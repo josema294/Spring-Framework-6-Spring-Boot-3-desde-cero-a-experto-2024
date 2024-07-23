95af6cd63caf94b6c0c5754e15a1e38a0698568c
Jose Maria Aranguren <64466787+josema294@users.noreply.github.com>
2024-07-23 22:24:09 +0200

    [clase 30]

--- cursospringboot/src/main/java/work/jmaranguren/cursospringboot/cursospringboot/controllers/PathVariableController.java
+++ cursospringboot/src/main/java/work/jmaranguren/cursospringboot/cursospringboot/controllers/PathVariableController.java
@@ -4,8 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+import java.util.HashMap;
 
 import work.jmaranguren.cursospringboot.cursospringboot.models.dto.ParamDTO;
+
 @RestController
 @RequestMapping("api/var")
 public class PathVariableController {
@@ -17,5 +20,15 @@ public class PathVariableController {
         param.setMessage(message);
         return param;
     }
+
+     @GetMapping("/mix/{product}/{id}")
+     public Map<String, Object> mixPathVar(@PathVariable String product,@PathVariable String id) {
+
+        Map<String, Object> json = new HashMap<>();
+        json.put("product",product);
+        json.put("id",id);
+        return json;
+     }
+     
 
 }
