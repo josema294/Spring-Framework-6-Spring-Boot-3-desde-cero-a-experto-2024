be3776807e53adf15e11cb63c4c67b0606969e09
Jose Maria Aranguren <64466787+josema294@users.noreply.github.com>
2024-07-27 21:25:50 +0200

    [Clase 43]

--- cursospringboot/src/main/java/work/jmaranguren/cursospringboot/controllers/HomeController.java
+++ cursospringboot/src/main/java/work/jmaranguren/cursospringboot/controllers/HomeController.java
@@ -3,8 +3,6 @@ package work.jmaranguren.cursospringboot.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import work.jmaranguren.cursospringboot.models.User;
-
 
 @Controller
 public class HomeController {
