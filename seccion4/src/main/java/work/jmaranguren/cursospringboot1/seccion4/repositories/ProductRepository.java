c9e7e0b45605ccc609ba33629d0e98bde4ad91c4
Jose Maria Aranguren <jmaranguren89@gmail.com>
2024-08-14 22:17:38 +0200

    [clase 45]

---
+++ seccion4/src/main/java/work/jmaranguren/cursospringboot1/seccion4/repositories/ProductRepository.java
@@ -0,0 +1,27 @@
+package work.jmaranguren.cursospringboot1.seccion4.repositories;
+
+import java.util.Arrays;
+import java.util.List;
+
+import work.jmaranguren.cursospringboot1.seccion4.models.Product;
+
+public class ProductRepository {
+
+    private List <Product> data;
+
+    public ProductRepository(List<Product> data) {
+        this.data = Arrays.asList(
+            new Product(1L, "Memoria cortsair 32", 300L),
+            new Product(2L, "Procesador intel", 900L),
+            new Product(3L, "HDD", 90L),
+            new Product(4L, "Teclado Razer", 60L)
+        );
+    }
+
+    public List<Product> findAll(){
+        return data;
+    }
+
+    
+
+}
