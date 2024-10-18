a01e5509c18f20f1986d9aa73dfc524663dafa5e
josema294 <jmaranguren89@gmail.com>
2024-10-18 21:08:51 +0200

    [clase 61]

---
+++ seccion4/src/main/java/work/jmaranguren/cursospringboot1/seccion4/appConfig.java
@@ -0,0 +1,10 @@
+package work.jmaranguren.cursospringboot1.seccion4;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:config.properties")
+public class appConfig {
+
+}
--- seccion4/src/main/java/work/jmaranguren/cursospringboot1/seccion4/services/ProductServiceImpl.java
+++ seccion4/src/main/java/work/jmaranguren/cursospringboot1/seccion4/services/ProductServiceImpl.java
@@ -6,6 +6,8 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import work.jmaranguren.cursospringboot1.seccion4.models.Product;
@@ -16,6 +18,10 @@ public class ProductServiceImpl implements ProductService{
         // @Autowired
         // @Qualifier("productFoo")
         private ProductRepository repository;
+        @Autowired
+        private Environment environment;
+        @Value("${config.price.tax}")
+        private double tax;
 
         // //@Autowired {por constructor, y de hecho no es necesario el @autowired si inyectas por constructor, ya que lo hace de forma automatica}
          public ProductServiceImpl(@Qualifier("productList")ProductRepository repository) {
@@ -36,7 +42,9 @@ public class ProductServiceImpl implements ProductService{
         // Retornamos una copia de los productos con el precio modificado
      
         return data.stream().map(p -> {
-            double conIVA = p.getPrice() *1.25;
+            System.out.println(environment.getProperty("config.price.tax"));
+            //double conIVA = p.getPrice() * environment.getProperty("config.price.tax", Double.class);
+            double conIVA = p.getPrice() * tax;
             // Product clonado = (Product) p.clonar();
             // clonado.setPrice(conIVA);
             // System.out.println("ProductServiceImpl.findAll()");
