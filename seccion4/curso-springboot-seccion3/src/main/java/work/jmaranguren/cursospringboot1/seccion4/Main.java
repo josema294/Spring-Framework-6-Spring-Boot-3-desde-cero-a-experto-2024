22f2ee60ce5ac0d9625f6270e9cd4d180dcd49b2
josema294 <jmaranguren89@gmail.com>
2024-10-20 20:24:31 +0200

    creada seccion 4, ejemplo factura

--- seccion4/curso-springboot-seccion3/src/main/java/work/jmaranguren/cursospringboot1/seccion4/Main.java
+++
@@ -1,7 +0,0 @@
-package work.jmaranguren.cursospringboot1.seccion4;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ seccion4/springboot-factura/src/main/java/work/jmaranguren/factura/springboot_factura/SpringbootFacturaApplication.java
@@ -0,0 +1,13 @@
+package work.jmaranguren.factura.springboot_factura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFacturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFacturaApplication.class, args);
+	}
+
+}
