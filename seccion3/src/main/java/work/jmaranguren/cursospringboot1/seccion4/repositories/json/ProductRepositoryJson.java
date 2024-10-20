fb8cecb83cd3441e567bf4058296fab6bdbe423c
josema294 <jmaranguren89@gmail.com>
2024-10-20 19:46:15 +0200

    Renombrado correcto

--- seccion3/src/main/java/work/jmaranguren/cursospringboot1/seccion4/repositories/json/ProductRepositoryJson.java
+++ seccion3/src/main/java/work/jmaranguren/cursospringboot1/seccion4/repositories/json/ProductRepositoryJson.java
@@ -41,7 +41,7 @@ public class ProductRepositoryJson implements ProductRepository {
     @Override
     public Product findById(Long id) {
 
-        return null;
+        return list.stream().filter( p -> p.getId().equals(id)).findFirst().orElseThrow();
     }
     
 }
