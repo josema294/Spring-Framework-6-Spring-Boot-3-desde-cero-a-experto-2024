16a3827b0d7494519e82e34718a4da6573e3bd59
josema294 <jmaranguren89@gmail.com>
2024-10-22 21:59:01 +0200

    [clase 72]

--- seccion4/springboot-factura/src/main/java/work/jmaranguren/factura/springboot_factura/AppConfig.java
+++ seccion4/springboot-factura/src/main/java/work/jmaranguren/factura/springboot_factura/AppConfig.java
@@ -21,5 +21,17 @@ List<Item> itemsFacturas (){
     Product p2 = new Product("Bicicleta ",1200);
     return Arrays.asList(new Item(p1,2), new Item(p2, 4));
 }
+
+@Bean("default")
+//@Primary
+List<Item> itemsFacturasOficina (){
+
+    Product p1 = new Product("Monitor Asus 24",800);
+    Product p2 = new Product("Notebook Razer ",1200);
+    Product p3 = new Product("Impresora HP",800);
+    Product p4 = new Product("Excriorio oficina ",900);
+    return Arrays.asList(new Item(p1,4), new Item(p2, 6),new Item(p3, 1),new Item(p4, 4));
+}
+
     
 }
